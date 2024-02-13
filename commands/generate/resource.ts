5c95d880368d9231ec3c7395afc3d1f82346fe64
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-02-13 21:49:14 +0100

    feat(command): implemented resource command generator

---
+++ commands/generate/resource.ts
@@ -0,0 +1,52 @@
+import { args, BaseCommand } from '@adonisjs/core/ace'
+import type { CommandOptions } from '@adonisjs/core/types/ace'
+import { readdir } from 'node:fs/promises'
+import { join } from 'node:path'
+
+const STUBS_ROOT = new URL('./stubs', import.meta.url)
+
+export default class GenerateModuleBase extends BaseCommand {
+  static commandName = 'generate:module'
+  static aliases = ['g:module']
+  static description = 'Generate a module declaration'
+
+  static options: CommandOptions = {}
+
+  @args.string()
+  declare name: string
+
+  async run() {
+    let domain: string
+    const domains = await readdir(join(process.cwd(), 'domains'))
+
+    if (!domains.length) {
+      domain = await this.prompt.ask('Enter the domain name', {
+        validate(value: string) {
+          return value.length > 0
+        },
+      })
+    } else {
+      domain = await this.prompt.choice('Select existing domain', domains)
+    }
+
+    const codemods = await this.createCodemods()
+    await Promise.all([
+      codemods.makeUsingStub(STUBS_ROOT.pathname, 'model.stub', {
+        name: this.name,
+        domain: domain,
+      }),
+      codemods.makeUsingStub(STUBS_ROOT.pathname, 'validator.stub', {
+        name: this.name,
+        domain: domain,
+      }),
+      codemods.makeUsingStub(STUBS_ROOT.pathname, 'policy.stub', {
+        name: this.name,
+        domain: domain,
+      }),
+      codemods.makeUsingStub(STUBS_ROOT.pathname, 'service.stub', {
+        name: this.name,
+        domain: domain,
+      }),
+    ])
+  }
+}
